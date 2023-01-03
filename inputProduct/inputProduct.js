cc9dcd2399024dd1704d135b9beef391ef4a918e
tan <buitan561993@gmail.com>
2023-01-03 14:57:28 +0700

    first commit

--- inputProduct/inputProduct.js
+++ inputProduct/inputProduct.js
@@ -1,29 +1,15 @@
 let localArr = localStorage.getItem("list_Product");
 let localData = JSON.parse(localArr)
-// function searchProduct() {
-//     let inputSearch=document.getElementById("searchItem").value;
-//     console.log(inputSearch)
-// else{
-//         let listSearch=[];
-//         for (let i = 0; i < localData.length; i++) {
-//             if (localData[i].name.toLowerCase().search(inputSearch.toLowerCase())!==-1){
-//                 listSearch.push(localData[i])
-//             }
-//         }
-//         drawTable(listSearch)
-//         console.log(listSearch)
-//     }
-// }
-//
 
 window.onload = drawNewProduct(localData)
+
 function drawNewProduct(arr) {
-    let count=Math.ceil(arr.length / 4)
+    let count = Math.ceil(arr.length / 4)
     let drawTable = "<table> <tr>";
     // console.log(count);
     for (let i = 0; i < count; i++) {
         for (let j = 1; j < 5; j++) {
-            if (i * 4 + j <= arr.length){
+            if (i * 4 + j <= arr.length) {
                 drawTable += `<td class="tableTd">
                                 <div class="divDisplay">
                                     <img src="${arr[i * 4 + j - 1].img} " class="imgDisplay">
@@ -34,9 +20,39 @@ function drawNewProduct(arr) {
                                 </div>
                                   
                             </td>`
-            }else  break
+            } else break
         }
         drawTable += "</tr>"
     }
     document.getElementById("show-product-here").innerHTML = drawTable;
 }
+
+// function searchProduct() {
+//     let inputSearch=document.getElementById("searchItem").value;
+//     let listProducts = document.querySelectorAll(".tableTd")
+//     console.log(listProducts)
+//     // console.log(listProducts[0].firstElementChild.children[2].innerText)
+//
+//         for (let i = 0; i < listProducts.length; i++) {
+//             if (listProducts[i].firstElementChild.children[1].innerText.toLowerCase().includes(inputSearch.toLowerCase())){
+//                 listProducts[i].style.display = "block"
+//             }
+//             else {
+//                 listProducts[i].style.display = "none"
+//                 listProducts[i].style.display = "none"
+//             }
+//         }
+// }
+function searchProduct() {
+    let inputSearch = document.getElementById("searchItem").value;
+    let listProduct = document.querySelectorAll(".tableTd")
+    console.log(listProduct[0].firstElementChild.children[1].innerText.toLowerCase())
+    for (let i = 0; i < listProduct.length; i++) {
+        if (listProduct[i].firstElementChild.children[1].innerText.toLowerCase().includes(inputSearch.toLowerCase())) {
+            listProduct[i].style.display = "block"
+        } else {
+            listProduct[i].style.display = "none"
+        }
+    }
+}
+
